8e2c085f8d05f2db147c2b55c917fd33952ed313
ranjan545486 <ranjan.mukherjee@hotmail.com>
2016-08-11 09:13:36 -0700

    Add files via upload

---
+++ MultiPutBlockingBoundedQueue.java
@@ -0,0 +1,7 @@
+package LinkedInQuestions;
+
+/**
+ * Created by rmukherj on 8/8/16.
+ */
+public interface MultiPutBlockingBoundedQueue<E> {
+}
--- PalindeRomeDP.java
+++ PalindeRomeDP.java
@@ -17,8 +17,8 @@ class PalindeRomeDP {
     public int solve() {
 
         // solve problems in correct order
-        for (int len = 2; len <= values.length; ++len) {
-            for (int i = 0; i < values.length - len; ++i) {
+        for (int len = 2; len <= values.length; len++) {
+            for (int i = 0; i < values.length - len; i++) {
                 solve(i, i+len-1);
             }
         }
@@ -42,6 +42,29 @@ class PalindeRomeDP {
 
     }
 
+   /*
+
+    */
+    public String longestPalindrome(String s) {
+        int start = 0, end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+    private int expandAroundCenter(String s, int left, int right) {
+        int L = left, R = right;
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++; }
+        return R - L - 1;
+    }
 
 
     public static void main(String[] args) {
